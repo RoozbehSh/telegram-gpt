c90d2e863456ac5e7d9f95f6d8b40b6af97cfb48
RoozbehSh <50093797+RoozbehSh@users.noreply.github.com>
2023-04-01 13:20:56 +0330

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use tg_flows::{listen_to_update, Telegram, UpdateKind};
 use openai_flows::{chat_completion, ChatOptions, ChatModel};
 use std::env;
+use teloxide::{prelude::*, utils::chat::get_chat_member};
 
 #[no_mangle]
 pub fn run() {
@@ -12,34 +13,38 @@ pub fn run() {
     let telegram_token = std::env::var("telegram_token").unwrap();
     let tele = Telegram::new(telegram_token.clone());
 
-    let target_channel = -1772546492; // Replace with your desired channel ID
-
     listen_to_update(telegram_token, |update| {
         if let UpdateKind::Message(msg) = update.kind {
             let text = msg.text().unwrap_or("");
             let chat_id = msg.chat.id;
 
-            let prompt = "You are a helpful assistant answering questions on Telegram.\n\nIf someone greets you without asking a question, you can simply respond \"Hello, I am your assistant on Telegram, built by the Second State team. I am ready for your question now!\"";
-            let co = ChatOptions {
-                model: ChatModel::GPT35Turbo,
-                restart: text.eq_ignore_ascii_case("restart"),
-                restarted_sentence: Some(prompt)
-            };
+            let target_channel = -1772546492; // Replace with the ID of your target channel.
+            let chat_member = get_chat_member(target_channel, msg.from().id).send().await;
 
-            // Check if user is a member of the target channel
-            let chat_member = tele.get_chat_member(target_channel, msg.from().id().unwrap());
-            if !chat_member.is_member() {
-                // User is not a member of the target channel
-                _ = tele.send_message(chat_id, "Please join our channel to ask questions.".to_string());
-                return;
-            }
+            match chat_member {
+                Ok(member) => {
+                    if member.status == teloxide::types::ChatMemberStatus::Member || member.status == teloxide::types::ChatMemberStatus::Administrator || member.status == teloxide::types::ChatMemberStatus::Creator {
+                        let prompt = "You are a helpful assistant answering questions on Telegram.\n\nIf someone greets you without asking a question, you can simply respond \"Hello, I am your assistant on Telegram, built by the Second State team. I am ready for your question now!\"";
+                        let co = ChatOptions {
+                            model: ChatModel::GPT35Turbo,
+                            restart: text.eq_ignore_ascii_case("restart"),
+                            restarted_sentence: Some(prompt)
+                        };
 
-            let c = chat_completion(&openai_key_name, &chat_id.to_string(), &text, &co);
-            if let Some(c) = c {
-                if c.restarted {
-                    _ = tele.send_message(chat_id, "I am starting a new conversation since it has been over 10 minutes from your last reply. You can also tell me to restart by typing \"restart\" into the chat.\n\n".to_string() + &c.choice);
-                } else {
-                    _ = tele.send_message(chat_id, c.choice);
+                        let c = chat_completion(&openai_key_name, &chat_id.to_string(), &text, &co);
+                        if let Some(c) = c {
+                            if c.restarted {
+                                _ = tele.send_message(chat_id, "I am starting a new conversation since it has been over 10 minutes from your last reply. You can also tell me to restart by typing \"restart\" into the chat.\n\n".to_string() + &c.choice);
+                            } else {
+                                _ = tele.send_message(chat_id, c.choice);
+                            }
+                        }
+                    } else {
+                        _ = tele.send_message(chat_id, "Please join the target channel to get your questions answered.".to_string());
+                    }
+                }
+                Err(e) => {
+                    println!("Error checking chat membership: {}", e);
                 }
             }
         }
